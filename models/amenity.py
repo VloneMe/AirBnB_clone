af94e60a42effb1f74257eaf51dec1eb2b22f670
KisingiMichael <kisingimichael12@gmail.com>
2023-08-13 18:32:41 -0700

    Add amenity

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module for the Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel
+    """
+    name = ""
