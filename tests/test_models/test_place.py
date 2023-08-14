53c1b0b074a0ffe3f5fa445be192a3058b893433
VloneMe <francisntima@yahoo.com>
2023-08-13 18:46:04 -0700

    test place class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Unit tests for the Place class
+"""
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """
+    Test suite for the Place class.
+    """
+
+    def test_attributes(self):
+        """
+        Test the initialization of Place attributes.
+        """
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
